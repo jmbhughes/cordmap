170408d63c2be74afe580dbf7acd425396609cf9
Marcus Hughes <hughes.jmb@gmail.com>
2023-08-01 10:26:31 -0600

    adds comments, fixes saving error
    Co-authored-by: Andres Muñoz-Jaramillo <amunozj@users.noreply.github.com>

--- bin/prepare_data.py
+++ bin/prepare_data.py
@@ -18,7 +18,19 @@ HIGH_PCT = 99.99
 
 THMAP_SIZE = 256
 
-def load_single_set(paths, directory):
+def load_single_set(paths:list(str), directory:str)->(np.ndarray, np.ndarray):
+    """Loads all channels and thematic for a single time
+
+    Args:
+        paths (list[str]): filenames to be loaded in order of 
+            94, 131, 171, 195, 284, 304, thematic map
+        directory (str): path that contains the data
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: 
+            first element is a cube of the SUVI observations and second is the thematic map
+            shapes are (6, 1280, 1280) and (256, 256)
+    """    
     cube = []
     for path in paths[:-1]:
         with fits.open(os.path.join(directory, path)) as hdul:
@@ -30,7 +42,24 @@ def load_single_set(paths, directory):
     return np.array(cube), thmap
     
 
-def load_cube(index, data_path, start, end):
+def load_all_sets(index: pd.DataFrame, 
+                  data_path: str, 
+                  start: int, 
+                  end: int) -> (np.ndarray, np.ndarray):
+    """Loads all sets and thematic maps defined in an index dataframe 
+        that are between start and end indices
+
+    Args:
+        index (pd.DataFrame): dataframe that links the different channel files together
+        data_path (str): directory that contains the data
+        start (int): end index
+        end (int): end index
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: 
+            first element is a cube of the SUVI observations and second is the thematic map
+            shapes are (N, 6, 1280, 1280) and (N, 256, 256) where N is the image set count
+    """    
     sub_index = index.loc[start:end]
     x, y = [], []
     
@@ -45,11 +74,38 @@ def load_cube(index, data_path, start, end):
          
     return np.array(x), np.array(y)
 
-def get_thresholds(cube, low_pct, high_pct, channel):
+def get_thresholds(cube: np.ndarray, 
+                   low_pct: float, 
+                   high_pct: float, 
+                   channel: int) -> (float, float):
+    """Given low and high percentiles, determines the corresponding values 
+        in an image cube for a given channel
+
+    Args:
+        cube (np.ndarray): cube of observations returned from `load_all_sets`
+        low_pct (float): low percentile
+        high_pct (float): high percentile
+        channel (_type_): index of the channel
+
+    Returns:
+        tuple[float, float]:
+            low percentile and high percentile value respectively
+    """    
     return (np.nanpercentile(cube[:, channel, :, :], low_pct), 
             np.nanpercentile(cube[:, channel, :, :], high_pct))
     
-def scale_data(cube, lows, highs):
+def scale_data(cube: np.ndarray, lows: [float], highs: [float]) -> np.ndarray:
+    """Scales data to the 0.25 power, linearizes between 0 and 255, and clips
+
+    Args:
+        cube (np.ndarray): cube of observations returned from `load_all_sets`
+        lows (list[floats]): low percentile values for each channel
+        highs (list[floats]): high percentile values for each channel
+
+    Returns:
+        np.ndarray: 
+            scaled data cube
+    """    
     cube = np.sign(cube) * np.power(np.abs(cube), SCALE_POWER)
         
     for i, (low, high) in enumerate(zip(lows, highs)):
@@ -71,14 +127,14 @@ if __name__ == "__main__":
     train_start = 0
     train_end = int(round(n * TRAIN_PCT))
     
-    valid_start = train_end + 1
+    valid_start = train_end
     valid_end = int(round(n * (TRAIN_PCT + VALID_PCT)))
     
-    test_start = valid_end + 1
+    test_start = valid_end
     test_end = n
         
     # load the data and scale it for each kind
-    train_x, train_y = load_cube(index, args.data_path, train_start, train_end)
+    train_x, train_y = load_all_sets(index, args.data_path, train_start, train_end)
     cube = np.sign(train_x) * np.power(np.abs(train_x), SCALE_POWER)
     
     lows, highs = [], []
@@ -89,19 +145,19 @@ if __name__ == "__main__":
         
     train_x = scale_data(train_x, lows, highs)
     
-    valid_x, valid_y = load_cube(index, args.data_path, valid_start, valid_end)
+    valid_x, valid_y = load_all_sets(index, args.data_path, valid_start, valid_end)
     valid_x = scale_data(valid_x, lows, highs)
     
-    test_x, text_y = load_cube(index, args.data_path, test_start, test_end)
+    test_x, test_y = load_all_sets(index, args.data_path, test_start, test_end)
     test_x = scale_data(test_x, lows, highs)
 
     # save everything!     
     zarr.save("/d0/mhughes/thmap_suvi_train_x.zarr", train_x)
     zarr.save("/d0/mhughes/thmap_suvi_train_y.zarr", train_y)
 
-    zarr.save("/d0/mhughes/thmap_suvi_valid_x.zarr", train_x)
-    zarr.save("/d0/mhughes/thmap_suvi_valid_y.zarr", train_y)
+    zarr.save("/d0/mhughes/thmap_suvi_valid_x.zarr", valid_x)
+    zarr.save("/d0/mhughes/thmap_suvi_valid_y.zarr", valid_y)
     
-    zarr.save("/d0/mhughes/thmap_suvi_test_x.zarr", train_x)
-    zarr.save("/d0/mhughes/thmap_suvi_test_y.zarr", train_y)
+    zarr.save("/d0/mhughes/thmap_suvi_test_x.zarr", test_x)
+    zarr.save("/d0/mhughes/thmap_suvi_test_y.zarr", test_y)
 
